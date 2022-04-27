d2e5c26782092212a6e0e2e85e038673f00adac7
Felipe <machadofelipe2016@outlook.com>
2022-04-27 17:25:46 -0300

    c1-create all states to caught hoock

--- contador/App.js
+++ contador/App.js
@@ -4,6 +4,30 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   const [num, setnumber] = useState(0)
+  const [running, setrunning] = useState(false)
+  const [time, settime] = useState(0)
+  
+  // create useEffect to manage hoock later loading page
+  useEffect(()=> {
+    let timer = null
+    if(running){
+      timer = setInterval(() => {
+         settime(old => old +1)
+      }, 1000);
+    }
+  })
+
+
+  const Increment = () => {
+    setnumber(num+1)
+  }
+  const Decrement = () => {
+    if(num >0){
+      setnumber(num-1)
+    }
+  }
+
+
   return (
     <View style={styles.container}>
       <Text> started new project counter number of steps </Text>
