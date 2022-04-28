b1083a311c10a00b9f08b68da7553397b22aaf57
Felipe <machadofelipe2016@outlook.com>
2022-04-28 14:53:34 -0300

    c7- change p to Text

--- contador/MostarTempo.js
+++ contador/MostarTempo.js
@@ -1,16 +1,16 @@
 import React from 'react'
-
+import {Text} from 'react-native'
 const MostrarTemp = (props) => {
     const tempo = props.tempo
     const minuto = Math.round(tempo / 60)
     const segundo = tempo % 60
     const minutoSeg = minuto < 10 ? '0' + minuto : segundo
     const segundoStr = segundo < 10? '0' + segundo : segundo 
-    return (
-        <p> {`${minutoSeg}: ${segundoStr}` } <br />
+    return ( <>
+        <Text> {`${minutoSeg}: ${segundoStr}` }  <br/>
       Tempo Médio por volta.
-      </p>
-    )
+      </Text>
+   </> )
 
 }
 
